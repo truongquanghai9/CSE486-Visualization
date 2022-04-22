b3519643cbbbcf456eab926c0b8ca1d66036921a
truongquanghai <truongquanghai9@gmail.com>
2022-04-22 14:14:10 -0700

    Remove unneeded codes

--- src/Visualization/Street/StreetInfo.js
+++ src/Visualization/Street/StreetInfo.js
@@ -1,17 +1,5 @@
 import React from 'react';
 
-// export interface Node {
-//     x: number,
-//     y: number,
-//     isStart: boolean,
-//     isGoal: boolean,
-//     length: number,
-//     isVisited: boolean,
-//     isWall: boolean,
-//     isVertical: boolean,
-//     name: string,
-//     adj: Street[]
-// }
 export interface Node {
   street: string;
   x: number;
